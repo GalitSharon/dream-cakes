8ed72de633121394683dfd76ee9d2cf4848f4cbe
OrDuan <galitsharon67@gmail.com>
2023-05-27 18:59:08 +0300

    clean up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,8 +11,8 @@ import * as Aos from 'aos';
 })
 export class AppComponent implements OnInit {
   title = 'dream-cakes';
-  @ViewChild('menuRef') menuRef!: 
-  ElementRef<HTMLInputElement>;
+  @ViewChild('menuRef') menuRef!:
+    ElementRef<HTMLInputElement>;
   @HostListener('window:scroll', ['$event'])
   onScroll(event: any) {
     Aos.init();
@@ -20,16 +20,11 @@ export class AppComponent implements OnInit {
     if (this.winRef.nativeWindow.pageYOffset === 0) {
       this.isMenuUp = true;
       this.renderer.removeClass(this.menuRef.nativeElement, 'menu-down');
-      // this.renderer.setStyle(this.menuRef.nativeElement, 'background-image', 'linear-gradient(to right, white 0%, white 5%, rgb(56, 55, 55) 5%, rgb(56, 55, 55) 45%, white 45%, white 55%, rgb(56, 55, 55) 55%, rgb(56, 55, 55) 95%, white 95%, white 100%)');
-
     } else {
       this.isMenuUp = false;
       this.renderer.addClass(this.menuRef.nativeElement, 'menu-down');
-      // this.renderer.setStyle(this.menuRef.nativeElement, 'trasition', 'background-image 20s');
-      // this.renderer.setStyle(this.menuRef.nativeElement, 'background-image', 'linear-gradient(to right, rgb(241, 240, 240) 0%, rgb(241, 240, 240) 100%)');
-      // this.renderer.setStyle(this.menuRef.nativeElement, 'trasition', 'background-image 20s');
     }
-    
+
   }
 
   isLoadingPage: boolean = true;
@@ -41,23 +36,10 @@ export class AppComponent implements OnInit {
   }
 
   ngOnInit() {
-   
+
     setTimeout(
-      () => { this.isLoadingPage = false; this.isAboutPage = true }, 1);
-      
+      () => { this.isLoadingPage = false; this.isAboutPage = true }, 3000);
+
   }
-
-  ngAfterViewInit() {
-    // ElementRef: { nativeElement: <input> }
-    // console.log(this.menuRef);
-
-    // Access the input object or DOM node
-    // console.dir(this.menuRef.nativeElement);
-
-    // Manipulate via Renderer2
-    
-  }
-
-
 
 }
