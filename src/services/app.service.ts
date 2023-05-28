bbe63f9cdd13f38dc5d6985cf58e83f8e77ea34b
OrDuan <galitsharon67@gmail.com>
2023-05-29 00:42:22 +0300

    add app service

---
+++ src/services/app.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { WindowRefService } from './window-ref.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AppService {
+  isMenuUp = true;
+  constructor(public winRef: WindowRefService) { }
+
+  updateMenuLocation(isUp: boolean) {
+    this.isMenuUp = isUp;
+  }
+
+  onWindowScroll() {
+    this.isMenuUp = this.winRef.nativeWindow.pageYOffset === 0;
+  }
+
+}
